19b14a520ffe45929ec2116d29cf2ef14cc7463f
labudu <kairosost38500@gmail.com>
2025-08-06 17:13:35 +0300

    Fix missing Routes import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import AuthPage from "./AuthPage";
 import HomePage from "./HomePage";
 
