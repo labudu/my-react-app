c1bfc57fbd39bf90c8f4cf4a71ab2a86195ac4ff
labudu <kairosost38500@gmail.com>
2025-08-06 17:00:23 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import AuthPage from "./AuthPage";
 import HomePage from "./HomePage";
 
